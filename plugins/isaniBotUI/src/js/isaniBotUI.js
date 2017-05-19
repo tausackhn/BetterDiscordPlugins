44fa950f6ad1ea1e044e37598ea9012f56b54374
BuildTools <namingray@gmail.com>
2017-05-19 14:37:31 +0300

    Test for external sources

--- plugins/isaniBotUI/src/js/isaniBotUI.js
+++ plugins/isaniBotUI/src/js/isaniBotUI.js
@@ -1,1 +1,17 @@
-module.exports.getName = () => 'isani-bot UI';
+const getName = () => 'isani-bot UI';
+const getDescription = () => 'Extends default discord UI with isani-bot functionality';
+const getVersion = () => '0.9.0';
+const getAuthor = () => 'Namingray';
+const onMessage = () => {};
+const onSwitch = () => {};
+const getSettingsPanel = () => '';
+
+export {
+  getName,
+  getDescription,
+  getVersion,
+  getAuthor,
+  onMessage,
+  onSwitch,
+  getSettingsPanel
+}
